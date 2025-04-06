fd3aae5b92f44ae80275e20d9c798977672edcc7
Md. Kamrul Hassan <98039269+Kamrul-hassan1438@users.noreply.github.com>
2025-04-06 18:35:55 +0600

    frontend commit 1

--- ChatApp/chat/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from chat.consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<sender>\w+)/(?P<recipient>\w+)/$', ChatConsumer.as_asgi()),
-]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Web_chating
-Real-time chatting  asgi Django 
